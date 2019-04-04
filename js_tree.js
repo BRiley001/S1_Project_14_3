254d1470e7741f49fba2f84901e827f9cdc04698
Brenden Riley <briley001@west-mec.org>
2019-04-04 15:22:13 -0700

    its all there
    Completed all the steps in the book, now just trying to decipher what the book said to do in such horrible wording that anyone would think to do something different than what the book actually wants you to do.

--- js_tree.js
+++ js_tree.js
@@ -45,12 +45,68 @@ var wsCount = 0;
 window.addEventListener("load", makeTree);
 
 function makeTree() {
-      var fragment = document.createDocumentFragment("< aside id='treeBox'> <h1> Node Tree </h1> </aside >")
+      var fragment = document.createElement("aside");
+      fragment.setAttribute("id", "treeBox");
+      var fragchild1 = document.createElement("h1");
+      fragment.appendChild(fragchild1);
+      fragchild1.appendChild(document.createTextNode("Node Tree"));
       document.getElementById("main").appendChild(fragment);
+
+      var nodeList = document.createElement("ol")
+      fragment.appendChild(nodeList);
+
+      var sourceArticle = document.querySelectorAll("#main article");
+
+      makeBranches(sourceArticle, nodeList);
+
+      document.getElementById("totalNodes").innerHTML = nodeCount;
+      document.getElementById("elemNodes").innerHTML = elemCount;
+      document.getElementById("textNodes").innerHTML = textCount;
+      document.getElementById("wsNodes").innerHTML = wsCount;
+}
+
+function makeBranches(treeNode, nestedList) {
+      nodeCount++;
+      var liElem = document.createElement("li");
+      var spanElem = document.createTextNode("+--<span></span>");
+      liElem.appendChild(spanElem);
+      nestedList.appendChild(liElem);
+
+      if (treeNode.nodeType == 1) {
+            console.log("this is true")
+            elemCount++;
+            spanElem.setAttribute("class", "elementNode");
+            spanElem.appendChild("<" + spanElem.nodeName.textContent + ">")
+      } else if (treeNode.nodeType == 2) {
+            console.log("this is true 2")
+            textCount++;
+            var textString = treeNode;
+            if (isWhiteSpaceNode(textString) == true) {
+                  console.log("this is true 3")
+                  wsCount++;
+                  spanElem.setAttribute("class", "whiteSpaceNode");
+                  var fragtext2 = "#text";
+                  spanElem.appendChild(fragtext2);
+            } else {
+                  console.log("this is true 4")
+                  spanElem.setAttribute("class", "textNode")
+                  spanElem.appendChild("" + textString + "")
+            }
+      }
+      if (document.getElementById("main").childNodes > 0) {
+            console.log("this is true 5")
+            var newList = document.createElement("ol");
+            newList.appendChild(document.createTextNode("|"));
+            nestedList.appendChild("newList");
+            for (var n = document.getElementById("main").firstChild; n !== null; n = n.nextSibling) {
+                  makeBranches(n, newList);
+            }
+      }
+
 }
 
 
-
+// PREMADE
 function isWhiteSpaceNode(tString) {
       return !(/[^\t\n\r ]/.test(tString));
 }
